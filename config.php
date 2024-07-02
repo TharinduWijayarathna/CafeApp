d940a87af98da919d6d1bb0bf16bf8ca79efb991
TharinduWijayarathna <tharinduwijayarathne87@gmail.com>
2024-07-02 16:14:37 +0530

    config changes

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "localhost";
+$host = "localhost";
 $username = "root";
 $password = "Wikum@123";
 $dbname = "gallery_cafe";
 
 // Create connection
-$database = new mysqli($servername, $username, $password, $dbname);
+$database = new mysqli($host, $username, $password, $dbname);
 
 // Check connection
 if ($database->connect_error) {
